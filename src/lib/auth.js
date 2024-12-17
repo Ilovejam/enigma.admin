2e8cc07bc6bfad7a59316a552a71319f63547e7c
Osman Melih Tolunay <78886346+Ilovejam@users.noreply.github.com>
2024-12-17 12:26:05 +0300

    add pw

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,5 +1,7 @@
 export const users = [
     { username: 'admin1', password: 'fb82ae3b35e126cf' },
     { username: 'admin2', password: 'password2' },
+    { username: 'özlemabla', password: 'Özlemabla123.' },
+
   ];
   
