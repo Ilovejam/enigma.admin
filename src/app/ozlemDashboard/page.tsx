bd3d3616694c2d42c9377c7714ff63d320d5871d
Osman Melih Tolunay <78886346+Ilovejam@users.noreply.github.com>
2024-12-26 18:28:19 +0300

    add balance

--- src/app/ozlemDashboard/page.tsx
+++ src/app/ozlemDashboard/page.tsx
@@ -44,17 +44,7 @@ export default function Dashboard() {
               Cüzdan
             </button>
             
-            <button
-              onClick={() => setActiveTab('trades')}
-              className={`flex items-center gap-2 px-4 py-2 rounded-lg transition-all ${
-                activeTab === 'trades'
-                  ? 'bg-purple-600 text-white'
-                  : 'bg-gray-700 text-gray-300 hover:bg-gray-600'
-              }`}
-            >
-              <FaMoneyBillWaveAlt />
-              İşlem Geçmişi
-            </button>
+           
           </nav>
         </div>
         <button
@@ -74,36 +64,6 @@ export default function Dashboard() {
 }
 
 function WalletSection({ username, password }: { username: string; password: string }) {
-  const [balance, setBalance] = useState(505.855);
-
-  useEffect(() => {
-    const fetchBalance = async () => {
-      try {
-        if (!username || !password) {
-          console.error('Kullanıcı adı veya şifre bulunamadı!');
-          return;
-        }
-
-        const response = await fetch('https://nox-admin-backend.vercel.app/api/getBalance', {
-          method: 'POST',
-          headers: { 'Content-Type': 'application/json' },
-          body: JSON.stringify({ username, password }),
-        });
-
-        const data = await response.json();
-
-        if (data.success) {
-          setBalance(data.balance);
-        } else {
-          console.error('API hatası:', data.message);
-        }
-      } catch (error) {
-        console.error('Error fetching balance:', error);
-      }
-    };
-
-    fetchBalance();
-  }, [username, password]);
 
   const handleCopy = () => {
     navigator.clipboard.writeText("0x96F7E183cacD630cfD3B85e91d01A8745D8669AA");
@@ -115,7 +75,7 @@ function WalletSection({ username, password }: { username: string; password: str
       <h1 className="text-4xl font-bold mb-6">Cüzdan</h1>
       <div className="bg-gray-800 p-6 rounded-lg shadow-lg text-center w-96">
         <h2 className="text-lg text-white font-bold">
-          Bakiye: <span className="text-green-500">${balance.toFixed(2)}</span>
+          Bakiye: <span className="text-green-500">$505.87</span>
         </h2>
         <div className="flex items-center justify-between mt-4">
           <p className="text-gray-400 text-sm">ERC20 Wallet Address:</p>
